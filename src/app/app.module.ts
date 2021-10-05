91f84b3d585ae66e154db4d9e1f463498530f439
paulo-vinicius23 <paulo.prado@aluno.uece.br>
2021-10-05 16:29:14 -0300

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { NaveBarComponent } from './nave-bar/nave-bar.component';
+
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataFormComponent,
+    NaveBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    NgxMaskModule.forRoot(maskConfig)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
